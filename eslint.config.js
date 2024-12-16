7fd001d7280ffe4c7aefbb087978d372b6c22ffb
Katrina Lopez <kkatlopez18@gmail.com>
2024-12-16 12:40:44 -0800

    fix dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,52 @@
+import js from '@eslint/js';
 import globals from 'globals';
-import pluginJs from '@eslint/js';
-import { pluginReact, react } from 'eslint-plugin-react';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
-/** @type {import('eslint').Linter.Config[]} */
 export default [
+    { ignores: ['dist'] },
     {
-        ignores: '**/node_modules/',
-    },
-    {
-        files: ['**/*.{js,mjs,cjs,jsx}'],
-    },
-    {
-        extends: [react, prettier],
-    },
-    {
-        languageOptions: { globals: globals.browser },
-    },
-    {
+        files: ['**/*.{js,jsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                ecmaFeatures: { jsx: true },
+                sourceType: 'module',
+            },
+        },
+        settings: { react: { version: '18.3' } },
+        plugins: {
+            react,
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+        },
         rules: {
+            ...js.configs.recommended.rules,
+            ...react.configs.recommended.rules,
+            ...react.configs['jsx-runtime'].rules,
+            ...reactHooks.configs.recommended.rules,
+            'react/jsx-no-target-blank': 'off',
+            'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+            'no-useless-catch': 'warn',
+            'no-redeclare': 'warn',
+            'no-undef': 'warn',
             'no-unused-vars': 'warn',
-            'prettier/prettier': 'warn',
+            'no-useless-escape': 'warn',
+            'react/no-unknown-property': 'warn',
+            'react/display-name': 'warn',
+            'react/prop-types': 'off',
+            'react/no-unescaped-entities': 'warn',
+            'react/jsx-key': 'warn',
+            'no-const-assign': 'warn',
+            'no-dupe-keys': 'warn',
+            'no-async-promise-executor': 'warn',
+            'no-prototype-builtins': 'warn',
             'react/no-children-prop': 'warn',
         },
     },
-    ...compat.extends('eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'),
+    eslintConfigPrettier,
 ];
